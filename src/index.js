b802b95dbb8a8af4ef356d5b27fc517e09b691bd
arnaulamiel <arnau.lamiel@estudiantat.upc.edu>
2021-06-07 20:47:41 +0200

    Pagina principal Voluntariapp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { App2 } from './App2';
+import { App } from './App';
+import {Esqueleto} from './Esqueleto'
 
-ReactDom.render(<App2 />, document.getElementById("root"))
+ReactDom.render(<Esqueleto />, document.getElementById("root"))
