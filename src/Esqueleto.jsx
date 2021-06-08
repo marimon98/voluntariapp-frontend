fec53e1df53bd102b5d071ccb90027eb7714ef2c
arnaulamiel <arnau.lamiel@estudiantat.upc.edu>
2021-06-08 18:44:17 +0200

    Barra lateral acabada

--- src/Esqueleto.jsx
+++ src/Esqueleto.jsx
@@ -4,9 +4,8 @@ import  {v4 as uuidv4} from 'uuid';
 import './Esqueleto.css';
 import { App } from './App';
 import {Link, Route, BrowserRouter as Router} from "react-router-dom";
-import homeIcon from './img/home.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faHands, faHandsHelping } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faHands, faHandsHelping, faUser,faTrophy, faCertificate, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import Navigation from "./components/Navigation";
 
 export function Esqueleto() {
@@ -75,7 +74,6 @@ export function Esqueleto() {
                                 <FontAwesomeIcon icon={faHands} />  &nbsp; &nbsp;
                                 <span>General</span>
                             </Link>
-
                         </li>
 
                         {/*<!-- Nav Item - Inscrits Collapse Menu -->*/}
@@ -85,70 +83,52 @@ export function Esqueleto() {
                                 <FontAwesomeIcon icon={faHandsHelping} />  &nbsp; &nbsp;
                                 <span>Inscrits</span>
                             </Link>
-                            <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
-                                data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <h6 className="collapse-header">Custom Utilities:</h6>
-                                    <a className="collapse-item" href="utilities-color.html">Colors</a>
-                                    <a className="collapse-item" href="utilities-border.html">Borders</a>
-                                    <a className="collapse-item" href="utilities-animation.html">Animations</a>
-                                    <a className="collapse-item" href="utilities-other.html">Other</a>
-                                </div>
-                            </div>
                         </li>
 
                         {/*<!-- Divider -->*/}
                         <hr className="sidebar-divider"/>
 
-                        {/*<!-- Heading -->*/}
+                        {/*<!-- Personal -->*/}
                         <div className="sidebar-heading">
-                            Addons
+                            Personal
                         </div>
 
-                        {/*<!-- Nav Item - Pages Collapse Menu -->*/}
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-                                aria-controls="collapsePages">
-                                <i className="fas fa-fw fa-folder"></i>
-                                <span>Pages</span>
-                            </a>
-                            <div id="collapsePages" className="collapse show" aria-labelledby="headingPages"
-                                data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <h6 className="collapse-header">Login Screens:</h6>
-                                    <a className="collapse-item" href="login.html">Login</a>
-                                    <a className="collapse-item" href="register.html">Register</a>
-                                    <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                                    <div className="collapse-divider"></div>
-                                    <h6 className="collapse-header">Other Pages:</h6>
-                                    <a className="collapse-item" href="404.html">404 Page</a>
-                                    <a className="collapse-item active" href="blank.html">Blank Page</a>
-                                </div>
-                            </div>
-                        </li>
 
-                        {/*<!-- Nav Item - Charts -->*/}
+                        {/*<!-- Nav Item - Perfil -->*/}
                         <li className="nav-item">
-                            <a className="nav-link" href="charts.html">
-                                <i className="fas fa-fw fa-chart-area"></i>
-                                <span>Charts</span></a>
+                            <Link className="nav-link collapsed" to="/profile" data-toggle="collapse" data-target="#collapseTwo"
+                                aria-expanded="true" aria-controls="collapseTwo">
+                                <FontAwesomeIcon icon={faUser} />  &nbsp; &nbsp;
+                                <span>Perfil</span>
+                            </Link>
                         </li>
-
-                        {/*<!-- Nav Item - Tables -->*/}
+                        {/*<!-- Nav Item - Experiencies -->*/}
                         <li className="nav-item">
-                            <a className="nav-link" href="tables.html">
-                                <i className="fas fa-fw fa-table"></i>
-                                <span>Tables</span></a>
+                            <Link className="nav-link collapsed" to="/experiences" data-toggle="collapse" data-target="#collapseTwo"
+                                aria-expanded="true" aria-controls="collapseTwo">
+                                <FontAwesomeIcon icon={faCertificate} />  &nbsp; &nbsp;
+                                <span>Experiències</span>
+                            </Link>
                         </li>
 
-                        {/*<!-- Divider -->*/}
+                        {/*<!-- Nav Item - Ranking -->*/}
+                        <li className="nav-item">
+                            <Link className="nav-link collapsed" to="/ranking" data-toggle="collapse" data-target="#collapseTwo"
+                                aria-expanded="true" aria-controls="collapseTwo">
+                                <FontAwesomeIcon icon={faTrophy} />  &nbsp; &nbsp;
+                                <span>Rànking</span>
+                            </Link>
+                        </li>
+                        { /*<!-- Divider -->*/}
                         <hr className="sidebar-divider d-none d-md-block"/>
-
-                        {/*<!-- Sidebar Toggler (Sidebar) -->*/}
-                        <div className="text-center d-none d-md-inline">
-                            <button className="rounded-circle border-0" id="sidebarToggle"></button>
-                        </div>
-
+                        {/*<!-- Nav Item - Sortir -->*/}
+                        <li className="nav-item">
+                            <Link className="nav-link collapsed" to="/login" data-toggle="collapse" data-target="#collapseTwo"
+                                aria-expanded="true" aria-controls="collapseTwo">
+                                <FontAwesomeIcon icon={faArrowLeft} />  &nbsp; &nbsp;
+                                <span>Sortir</span>
+                            </Link>
+                        </li>
                     </ul>
                     {/*<!-- End of Sidebar -->*/}
 
@@ -380,7 +360,7 @@ export function Esqueleto() {
                         <footer className="sticky-footer bg-white">
                             <div className="container my-auto">
                                 <div className="copyright text-center my-auto">
-                                    <span>Copyright &copy; Your Website 2020</span>
+                                    <span>Copyright &copy; VoluntariApp 2021</span>
                                 </div>
                             </div>
                         </footer>
