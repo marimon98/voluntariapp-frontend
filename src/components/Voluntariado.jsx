1a7ff73cfbf85e2f87c6c494ebc1224af13d0250
Fernando Marimon <marimon98@gmail.com>
2021-06-08 18:41:59 +0200

    Pruebas de estilo, se ve feo...

--- src/components/Voluntariado.jsx
+++ src/components/Voluntariado.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
-function Voluntariado({voluntariado}) {
+function Voluntariado({voluntariado, handleInscription}) {
 
     const {id, title, body, image, owner} = voluntariado;
 
+    const inscription = () => {
+        handleInscription(id);
+    }
+
     return (
         <div>
-            <h2>{title}</h2>
+            <h1 style={mystyle}>{title}</h1>
             {body} <br/>
             {owner} <br/>
             {}
         </div>
     );
 }
+
+const mystyle = {
+    color: "orange",
+    padding: "5px",
+    fontSize: 25
+};
 
 export default Voluntariado;
