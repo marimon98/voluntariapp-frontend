3007436aff4bfd266b013cba89a7a94c7c3b6d78
Fernando Marimon <marimon98@gmail.com>
2021-06-14 20:51:20 +0200

    nombres empresa home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import imag from "./img/imagenPlaceHolder.png"
 
 function Home () {
     const [publicaciones, setPublicaciones] = useState([
-        {id:1, body: "Esteu atents perque en un parell de dies obrirem nous voluntariats per Barcelona i als voltants per a ajudar a protectores d'animals. No us ho perdeu!", image: null, owner: "Damm-BCN", data: "11/05/2021"},
-        {id:2, body: "Descripció de publicació exemple.", image: imag, owner: "Damm",  data: "xx/xx/20xx"},
-        {id:3, body: "La cursa del divendres passat va ser tot un èxit i la gent va quedar molt contenta. Esperem que en les properes també tinguem aquesta participació o més.", image: null, owner: "Damm-Tortosa",  data: "20/02/2021"}
+        {id:1, body: "Esteu atents perque en un parell de dies obrirem nous voluntariats per Barcelona i als voltants per a ajudar a protectores d'animals. No us ho perdeu!", image: null, owner: "Damm", data: "11/05/2021"},
+        {id:2, body: "Descripció de publicació exemple.", image: imag, owner: "EmpresaX",  data: "xx/xx/20xx"},
+        {id:3, body: "La cursa del divendres passat va ser tot un èxit i la gent va quedar molt contenta. Esperem que en les properes també tinguem aquesta participació o més.", image: null, owner: "Coca-Cola Co.",  data: "20/02/2021"}
     ]);
 
     return (
